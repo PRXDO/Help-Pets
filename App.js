8be68e2247653f1afe50d619d6f46d2219ed5929
keilaemy <keilataniguchi@alunos.utfpr.edu.br>
2021-05-20 20:24:54 -0300

    add fix e tela Alimentacao pronta

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import Routes from './src/router';
-console.disableYellowBox = true;
+import Routes from "./src/router";
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" backgroundColor="#000" translucent={true} />
-      <Routes/>
+      <Routes />
     </>
   );
 }
@@ -16,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/router.js
+++ src/router.js
@@ -1,49 +1,48 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Menu from "./component/DrawerMenu";
 
-import Login from './pages/Login';
-import Home from './pages/Home';
-import NewPet from './pages/NewPet';
-import Register from './pages/Register';
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NewPet from "./pages/NewPet";
+import Register from "./pages/Register";
 
 const Stack = createStackNavigator();
-console.disableYellowBox = true;
-function Routes(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-            <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{headerShown: false}}
-                />
-                <Stack.Screen
-                    name="Menu"
-                    component={Menu}
-                    options={{headerShown: false}}
-                />
-                <Stack.Screen
-                    name="NewPet"
-                    component={NewPet}
-                    options={{headerShown: false}}
-                />
-                <Stack.Screen
-                    name="Home"
-                    component={Home}
-                    options={{headerShown: false}}
-                />
-                <Stack.Screen
-                    name="Register"
-                    component={Register}
-                    options={{headerShown: false}}
-                />
 
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Menu"
+          component={Menu}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NewPet"
+          component={NewPet}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default Routes;
